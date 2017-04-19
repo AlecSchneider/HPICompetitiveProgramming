66bcec148913b2940d270402bd8784982e9802d1
Alec Schneider <alecschneider@me.com>
2017-04-19 19:16:14 +0200

    :tada: solved problem in week1

---
+++ Week1/Week1/main.cpp
@@ -0,0 +1,99 @@
+//
+//  main.cpp
+//  Week1
+//
+//  Created by Alec Schneider on 19.04.17.
+//  Copyright © 2017 Alec Schneider. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::set<int> si;
+typedef std::map<char, int> msi;
+
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define REP(i, a, b) \
+for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
+#define TRvi(c, it) \
+for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define INF 2000000000 // 2 billion
+
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+
+
+#pragma mark - MAIN
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    std::ios::sync_with_stdio(false);
+    
+    #ifdef USE_INPUT_FILE
+    freopen("input.txt", "r", stdin);
+    #endif
+    #ifdef USE_OUTPUT_FILE
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    // MAIN Begin
+    
+    int cases;
+    cin >> cases; // read n
+    
+    for (int i=0; i<cases; i++) {
+        
+        int res = 1;
+        msi dict;
+        string m;
+        string n;
+        cin >> m; // read m
+        cin >> n;
+        if (m.length() == n.length()) {
+            for (int j = 0; j < m.length(); j++) {
+                dict[m[j]] += 1;
+            }
+            for (int j = 0; j < n.length(); j++) {
+                msi::iterator value = dict.find(n[j]);
+                if (value != dict.end() && value->second > 0) {
+                    dict[n[j]] -= 1;
+                }
+                else {
+                    res = 0;
+                    break;
+                }
+            }
+        }
+        else {
+            res = 0;
+        }
+        
+        
+        cout << res << endl; // write result on stdout
+    }
+    
+    // MAIN End
+    
+    #ifdef USE_OUTPUT_FILE
+    freopen("diff.txt", "w", stdout);
+    system("diff output.txt expected_output.txt");
+    #endif
+    
+    return 0;
+}
